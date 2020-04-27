8a430f2081e6288b0aa5681d8da3a0d51d289a69
HelenaGH <47184541+HelenaGH@users.noreply.github.com>
2020-04-27 10:14:19 +0200

    WorkInProgress

---
+++ Shooter/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour 
+{
+	public Transform meta;
+	public float filtriranje = 5f;
+
+	Vector3 pomak;
+
+	// Use this for initialization
+	void Start () 
+	{
+		pomak = transform.position - meta.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		Vector3 targetCamPos = meta.position + pomak;
+		transform.position = Vector3.Lerp(transform.position, targetCamPos, filtriranje * Time.deltaTime); 
+	}
+}
---
+++ Shooter/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float brzina = 6f;
+    Vector3 kretanje;
+    Animator anim;
+    Rigidbody igracevRigidbody;
+    int podnaMaska;
+    float camRayLength = 100f;
+
+    void Awake()
+    {
+        podnaMaska = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        igracevRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Move(h, v);
+        Turning();
+        Animating(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        kretanje.Set(h, 0f, v);
+        kretanje = kretanje.normalized * brzina * Time.deltaTime;
+        igracevRigidbody.MovePosition(transform.position + kretanje);
+    }
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit podPogodak;
+
+        if (Physics.Raycast(camRay, out podPogodak, camRayLength, podnaMaska))
+        {
+            Vector3 odIgracaDoMisa = podPogodak.point - transform.position;
+            odIgracaDoMisa.y = 0f;
+            Quaternion novaRotacija = Quaternion.LookRotation(odIgracaDoMisa);
+            igracevRigidbody.MoveRotation(novaRotacija);
+        }
+    }
+
+    void Animating(float h, float v)
+    {
+        bool hoda = h != 0f || v != 0f;
+        anim.SetBool("DaHoda", hoda);
+    }
+   
+}
